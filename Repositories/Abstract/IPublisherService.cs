78a097c5989aba864b253e30f69d0573948326ca
olakowall <k.magdalena.aleksandra@gmail.com>
2023-03-02 20:57:17 +0100

    adding almost all controllers and views

---
+++ Repositories/Abstract/IPublisherService.cs
@@ -0,0 +1,12 @@
+﻿using BookStoreApp.Models.Domain;
+namespace BookStoreApp.Repositories.Abstract
+{
+    public interface IPublisherService
+    {
+        bool Add(Publisher model);
+        bool Update(Publisher model);
+        bool Delete(int id);
+        Publisher FindById(int id);
+        IEnumerable<Publisher> GetAll();
+    }
+}
